8d46eb4ed7bbf6658f2f601d27bb43670bbc110f
jkjoseph2023@gmail.com <jkjoseph2023@gmail.com>
2025-01-30 00:28:37 +0530

    JWT Implmented

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,10 +1,22 @@
-﻿namespace BuberDinner.Application.Services.Authentication
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication
 {
-    public class AuthenticationService : IAuthenticationService
+    public class AuthenticationService(IJwtTokenGenerator _jwtTokenGenerator) : IAuthenticationService
     {
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
-            return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, password);
+            //Check if user already exisits
+
+
+
+            //Create User (Generate unique ID)
+
+            //Generate JWT
+            Guid userId = Guid.NewGuid();
+            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+            return new AuthenticationResult(userId, firstName, lastName, email, token);
         }
 
         public AuthenticationResult Login(string email, string password)
---
+++ BuberDinner.Infrastructure/Authentication/JwtTokenGeneratore.cs
@@ -0,0 +1,30 @@
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BuberDinner.Infrastructure.Authentication
+{
+    public class JwtTokenGeneratore : IJwtTokenGenerator
+    {
+        public string GenerateToken(Guid userId, string firstName, string lastName)
+        {
+            var siginingCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this-is-a-very-strong-secret-key!123456")), SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+                new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var securityToken = new JwtSecurityToken(
+                issuer: "BubberDinner",
+                expires: DateTime.Now.AddDays(1),
+                claims: claims,
+                signingCredentials: siginingCredentials);
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+    }
+}
