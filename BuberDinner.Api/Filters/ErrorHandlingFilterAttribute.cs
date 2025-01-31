6acc1b3e222af483c5676e2ec35e448d4bec2793
jkjoseph2023@gmail.com <jkjoseph2023@gmail.com>
2025-01-31 20:58:18 +0530

    Global Exception Handling

---
+++ BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace BuberDinner.Api.Filters
+{
+    public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+            var problemDetails = new ProblemDetails
+            {
+                Type = "www.google.com",
+                Title = "Ann Error Occured While Processing Request",
+                Detail = exception.Message,
+                Status = (int)HttpStatusCode.InternalServerError,
+                 
+            };
+            context.Result = new ObjectResult(problemDetails)
+            {
+                StatusCode = 500
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,14 +1,19 @@
+using BuberDinner.Api.Filters;
+using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplication().AddInfrastructure(builder.Configuration);
+    //builder.Services.AddControllers(option => option.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
 }
 
 var app = builder.Build();
 {
+    app.UseMiddleware<ErrorHandlingMiddleware>();
+    //app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
